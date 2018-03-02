e73b31b33f6ee3d69f323592cb744538d07c48a1
lxzzzzzz <mekor@live.cn>
2018-03-02 22:26:56 +0800

    添加ip视图

---
+++ PersonalMusic/Classes/Download/View/DownloadTipView.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadTipView.swift
+//  PersonalMusic
+//
+//  Created by 李小争 on 2018/3/2.
+//  Copyright © 2018年 Citynight. All rights reserved.
+//
+
+import UIKit
+
+class DownloadTipView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
