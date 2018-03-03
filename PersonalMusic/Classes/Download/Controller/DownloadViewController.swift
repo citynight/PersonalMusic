5c713b273cf49af085bbefb12334fac330c22b1c
lxzzzzzz <mekor@live.cn>
2018-03-03 17:05:26 +0800

    显示电脑下载

--- PersonalMusic/Classes/Download/Controller/DownloadViewController.swift
+++ PersonalMusic/Classes/Download/Controller/DownloadViewController.swift
@@ -17,11 +17,34 @@ class DownloadViewController: UIViewController {
         return view
     }()
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: self.view.bounds)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    private var webServer: GCDWebUploader?
+    private lazy var dataSource:[String] = []
+    
+    private lazy var documentsPath: String = {
+        guard let documentsPathStr = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentationDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true).first else {
+            return ""
+        }
+        let documentsPath = documentsPathStr + "/musics"
+        if !FileManager.default.fileExists(atPath: documentsPath) {
+            try? FileManager.default.createDirectory(atPath: documentsPath, withIntermediateDirectories: true, attributes: nil)
+        }
+        return documentsPath
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        fetchFileNames()
     }
     deinit {
+        webServer?.stop()
         print("jieshu")
     }
 }
@@ -29,8 +52,10 @@ extension DownloadViewController {
     
     func setupUI() {
         view.backgroundColor = UIColor.white
+        configTableView()
         configIpView()
         configNav()
+        
     }
     func configNav() {
         let rightBarButtonItem = UIBarButtonItem(title: "从电脑导歌", style: .plain, target: self, action: #selector(downloadMusic))
@@ -39,34 +64,37 @@ extension DownloadViewController {
         self.navigationController?.setTitleColor(UIColor.black)
     }
     
+    func configTableView() {
+        view.addSubview(tableView)
+        tableView.registerClassFromClass(type: UITableViewCell.self)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
+    }
+    
     func configIpView() {
         view.addSubview(downloadTipView)
+        downloadTipView.isHidden = true
         downloadTipView.snp.makeConstraints { (make) in
             make.left.right.bottom.equalTo(self.view)
             make.height.equalTo(250)
         }
-        downloadTipView.closeComplete = {[weak self] in
-            guard let `self` = self else {
-                return
-            }
-            self.downloadTipView.isHidden = true
-        }
+    }
+    
+    func fetchFileNames() {
+        let subpaths = FileManager.default.subpaths(atPath: documentsPath)
+        dataSource = subpaths ?? []
+        tableView.reloadData()
     }
 }
 
 @objc
 extension DownloadViewController {
     func downloadMusic() {
-        guard let documentsPathStr = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentationDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true).first else {
-            return
+        downloadTipView.isHidden = false
+        if webServer == nil {
+            webServerConfig(with: documentsPath)
         }
-        let documentsPath = documentsPathStr + "/musics"
-        if !FileManager.default.fileExists(atPath: documentsPath) {
-            try? FileManager.default.createDirectory(atPath: documentsPath, withIntermediateDirectories: true, attributes: nil)
-        }
-        print(documentsPath)
-        
-        webServerConfig(with: documentsPath)
     }
 }
 
@@ -77,26 +105,50 @@ extension DownloadViewController {
         webServer.allowHiddenItems = true
         if webServer.start() {
             print("服务器启动")
-            downloadTipView.isHidden = false
+            let ip = webServer.serverURL?.absoluteString
+            downloadTipView.ipLabel.text = ip
         }else {
             print("启动失败")
             downloadTipView.isHidden = true
+        }
+        self.webServer = webServer
+        downloadTipView.closeComplete = {[weak self] in
+            guard let `self` = self else {
+                return
+            }
+            webServer.stop()
+            self.webServer = nil
+            self.downloadTipView.isHidden = true
         }
     }
 }
 extension DownloadViewController: GCDWebUploaderDelegate {
     func webUploader(_ uploader: GCDWebUploader, didUploadFileAtPath path: String) {
         print("upload", path)
+        fetchFileNames()
     }
     
     func webUploader(_ uploader: GCDWebUploader, didMoveItemFromPath fromPath: String, toPath: String) {
         print("MOVE",fromPath,toPath)
+        fetchFileNames()
     }
     
     func webUploader(_ uploader: GCDWebUploader, didDeleteItemAtPath path: String) {
         print("DELETE",path)
+        fetchFileNames()
     }
     func webUploader(_ uploader: GCDWebUploader, didCreateDirectoryAtPath path: String) {
         print("CREATE",path)
+        fetchFileNames()
+    }
+}
+extension DownloadViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSource.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(type: UITableViewCell.self, forIndexPath: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
     }
 }
